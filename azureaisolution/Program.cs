613fd554081038ff1a3810cbc9809827028d6632
Swapnil Gaikwad <swpnlgkwd@hotmail.com>
2023-08-19 05:38:53 +0530

    Added Speech to Text Code

--- azureaisolution/Program.cs
+++ azureaisolution/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+﻿
+// Added for IConfigurationBuilder 
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace azureaisolution
+{
+    internal class Program
+    {
+        // Configuration required to connect to Cognitive Service
+        private static string? cogSvcEndpoint;
+        private static string? cogSvcKey;
+        private static string? region;
+
+        static async Task Main(string[] args)
+        {
+            // Load and Display Configuration
+            LoadConfiguration();
+            DisplayConfigurationValues();
+
+            var configurationIsValid = region != null && cogSvcEndpoint != null && cogSvcKey != null;
+
+            if (configurationIsValid)
+            {
+                Console.WriteLine("Speech to Text Service");
+                await SpeechToTextPractice.ConvertWavFileSpeechToText(cogSvcKey, region);
+            }
+
+        }
+        public static void LoadConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            IConfigurationRoot configuration = builder.Build();
+            cogSvcEndpoint = configuration["CognitiveServiceEndpoint"];
+            cogSvcKey = configuration["CognitiveServiceKey"];
+            region = configuration["CognitiveServiceRegion"];
+        }
+
+        private static void DisplayConfigurationValues()
+        {
+            Console.WriteLine("Cognitive Service Configuration:");
+            Console.WriteLine($"ServiceEndPoint: {cogSvcEndpoint}");
+            Console.WriteLine($"ServiceKey: {cogSvcKey}");
+            Console.WriteLine($"ServiceRegion: {region}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ azureaisolution/SpeechToTextPractice.cs
@@ -0,0 +1,103 @@
+﻿// Needed for SpeechConfig
+using Microsoft.CognitiveServices.Speech;
+// Needed for AudioConfig
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azureaisolution
+{
+    // Responsible for handling Speech to Text Conversion
+    static class SpeechToTextPractice
+    {
+        // Speech to Text needs SpeechConfig and AudioConfig
+        static SpeechConfig? speechConfig;
+        static AudioConfig? audioConfig;
+        static SpeechRecognizer? speechRecognizer;
+        static SpeechRecognitionResult? speechRecognitionResult;
+
+        public static async Task ConvertWavFileSpeechToText(string cogSvcKey, string cogSvcRegion)
+        {
+            try
+            {
+                // SpeechConfig is Required to connect to your Speech Resource
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+
+                // Source File
+                string audioFileInput = @"assets\speechtotext\time.wav";
+
+                // Input Source 
+                audioConfig = AudioConfig.FromWavFileInput(audioFileInput);
+
+                // This is not required and only for Demo purpose
+                //SoundPlayer wavPlayer = new(audioFileInput);
+                //wavPlayer.Play();
+
+                speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+                speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+
+                if (speechRecognitionResult.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine("Text : " + speechRecognitionResult.Text);
+                    Console.WriteLine("Duration : " + speechRecognitionResult.Duration);
+                    Console.WriteLine("OffsetInTicks : " + speechRecognitionResult.OffsetInTicks);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public static async Task ConvertSpeechToText(string cogSvcKey, string cogSvcRegion)
+        {
+            try
+            {
+                // SpeechConfig is Required to connect to your Speech Resource
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+
+                // Source File
+                string audioFileInput = @"assets\speechtotext\time.wav";
+
+                // Input Source 
+                audioConfig = AudioConfig.FromWavFileInput(audioFileInput);
+
+                // This is not required and only for Demo purpose
+                //SoundPlayer wavPlayer = new(audioFileInput);
+                //wavPlayer.Play();
+
+                speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+                speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+
+                if (speechRecognitionResult.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine("Text : " + speechRecognitionResult.Text);
+                    Console.WriteLine("Duration : " + speechRecognitionResult.Duration);
+                    Console.WriteLine("OffsetInTicks : " + speechRecognitionResult.OffsetInTicks);
+                }
+                else
+                {
+                    Console.WriteLine(speechRecognitionResult.Reason);
+                    if (speechRecognitionResult.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                        Console.WriteLine(cancellation.Reason);
+                        Console.WriteLine(cancellation.ErrorDetails);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
